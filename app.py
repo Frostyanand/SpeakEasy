09caa09ab123d67ba7f962b62e72068f21047fc8
Anurag Anand <154767320+Frostyanand@users.noreply.github.com>
2025-04-29 18:30:54 +0530

    Update app.py for render deploy

--- app.py
+++ app.py
@@ -28,5 +28,9 @@ app.register_blueprint(booking_bp, url_prefix='/api')
 def health_check():
     return "SpeakEasy API is running."
 
+#if __name__ == '__main__':
+#    app.run(debug=True) 
+
 if __name__ == '__main__':
-    app.run(debug=True) 
+    port = int(os.environ.get("PORT", 5000))
+    app.run(debug=True, host='0.0.0.0', port=port)
